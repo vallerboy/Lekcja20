79b8deabf79fccac6a6e4c12db0775e6a81be5d9
vallerboy <oskarbank@gmail.com>
2016-09-28 19:56:55 +0200

    HttpConnect client

--- app/src/main/java/pl/oskarpolak/json/MainActivity.java
+++ app/src/main/java/pl/oskarpolak/json/MainActivity.java
@@ -18,9 +18,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+       new ConnectToWWW().execute();
+    }
+
+    private void readJSON(String s){
+
+        try {
+            JSONArray rootArray = new JSONArray(s);
+
+            for(int i = 0; i <= rootArray.length()-1; i++){
+                JSONObject object = rootArray.getJSONObject(i);
+
+                Log.e("JSON", object.getString("guid"));
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
+    private class ConnectToWWW extends AsyncTask<Void, String, String>{
+
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpConnect connect = new HttpConnect();
+            String ourWebsite = connect.makeCall("http://www.json-generator.com/api/json/get/bQFBDNeEpu?indent=2");
+            return ourWebsite;
+        }
+
+        @Override
+        protected void onPostExecute(String ourText) {
+            readJSON(ourText);
+        }
+    }
 
 
 
